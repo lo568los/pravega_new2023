cddb9231f412518ef9e94a3049de91f7599d11f9
Shreshthallbestno1 <36707891+Shreshthallbestno1@users.noreply.github.com>
2024-01-21 08:43:21 +0530

    pronites page

--- Rockfest/Rockfest HTML/js/countdown-custom.js
+++ Rockfest/Rockfest HTML/js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2023, 8, 20, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2024, 0, 26, 19)}); // year, month, date, hour
         });
 });		
 
